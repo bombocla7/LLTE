4109e665f644bf0f6093c520ba9f8fe5708f1b08
ToNiNo324 <238039202+ToNiNo324@users.noreply.github.com>
2026-02-21 03:43:30 +0100

    Lanzamiento de flecha acabado

--- lanzando-lanzando-triunfe-escalando/arco.gd
+++ lanzando-lanzando-triunfe-escalando/arco.gd
@@ -1,40 +1,47 @@
 extends Node2D
 
-@export var max_carga = 500
+@export var max_carga = 100
+@export var arrow_scene: PackedScene
 
 var wasclicked = false
-
+var flecha
 var carga = 0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	look_at_mouse()
 	
-	#Carga del arco
+	#Carga del arco, cuando llega al máximo se queda esperando a que se suelte
 	if Input.is_action_pressed("ui_click"):
 		if (carga < max_carga):
 			arco_carga()
-		elif (carga == max_carga):
-			arco_disparo()
-	#Disparo del arco
-	elif Input.is_action_just_released("ui_click"):
-		arco_disparo()
 		
-	#Debug carga
-	print(carga)
+	#Disparo del arco
+	if Input.is_action_just_released("ui_click"):
+		arco_disparo(carga)
+		print(str(flecha.velocity.x))
+		print(str(flecha.velocity.y))
 
 func look_at_mouse():
 	var mouse_pos = get_global_mouse_position()
 	look_at(mouse_pos)
 
 func arco_carga():
-	if !wasclicked:
-		wasclicked = true 
-	else:
-		carga += 1
+	carga += 1
+	print(carga)
 
-func arco_disparo():
+func arco_disparo(carga_disparo):
 	#Dispara la flecha con velocidad proporcional a la carga y en la dirección del ratón
-	
-	#Resetea la carga y el click para el siguiente frame
+	flecha = arrow_scene.instantiate()
+	get_tree().current_scene.add_child(flecha)
+	var direccion_x = (get_global_mouse_position() - global_position).x
+	var direccion_y = (get_global_mouse_position() - global_position).y
+	print(str(direccion_x))
+	print(str(direccion_y))
+	var fuerza_base = 50.0 
+	#EL OBJETO A CREAR NO DEBE TENER LA VELOCIDAD EN X DEFINIDA EN 0 O EN OTRO VALOR ESTRICTO, LA VELOCIDAD DE LA FLECHA SE DA AQUI
+	flecha.velocity.x += direccion_x * fuerza_base * carga_disparo * 0.01
+	flecha.velocity.y = direccion_y * fuerza_base * carga_disparo * 0.01
+	var direccion = Vector2(direccion_x, direccion_y)
+	flecha.rotation = direccion.angle()
 	wasclicked = false
 	carga = 0
