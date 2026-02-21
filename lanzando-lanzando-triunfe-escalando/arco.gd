1461ce157512dc752ef1256274de95365d3f3af1
ToNiNo324 <238039202+ToNiNo324@users.noreply.github.com>
2026-02-21 20:42:46 +0100

    animacion de arco y flecha

--- lanzando-lanzando-triunfe-escalando/arco.gd
+++ lanzando-lanzando-triunfe-escalando/arco.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var max_carga = 150 # 150 = 3 segundos
 @export var arrow_scene: PackedScene
 
+@onready var animacion = $"Arco (sprite)"
+
 var wasclicked = false
 var flecha
 var carga = 0
@@ -10,14 +12,18 @@ var carga = 0
 func _physics_process(delta):
 	look_at_mouse()
 	
+	
 	#Carga del arco, cuando llega al máximo se queda esperando a que se suelte
 	if Input.is_action_pressed("ui_click"):
 		if (carga < max_carga):
 			arco_carga()
 		
+	else:
+		animacion.play("idle")
 	#Disparo del arco
 	if Input.is_action_just_released("ui_click"):
 		arco_disparo(carga)
+		animacion.play("idle")
 
 
 func look_at_mouse():
@@ -25,23 +31,30 @@ func look_at_mouse():
 	look_at(mouse_pos)
 
 func arco_carga():
-	carga += 1
+	animacion.play("cargado")
+	carga += 2.5
 	print(carga)
 
 func arco_disparo(carga_disparo):
 	#Dispara la flecha con velocidad proporcional a la carga y en la dirección del ratón
-	if flecha != null:
-		flecha.queue_free()
-	
-	flecha = arrow_scene.instantiate()
-	get_tree().current_scene.add_child(flecha)
-	flecha.global_position = global_position
-	if get_parent().has_method("conectar_extremo"):
-		get_parent().conectar_extremo(flecha)
-	var direccion_x = (get_global_mouse_position() - global_position).normalized()
-	print(str(direccion_x))
-	var fuerza_base = 5
-	flecha.rotation = direccion_x.angle()
-	#EL OBJETO A CREAR NO DEBE TENER LA VELOCIDAD EN X DEFINIDA EN 0 O EN OTRO VALOR ESTRICTO, LA VELOCIDAD DE LA FLECHA SE DA AQUI
-	flecha.linear_velocity = direccion_x * fuerza_base * carga_disparo
-	carga = 0
+	if flecha == null:
+		flecha = arrow_scene.instantiate()
+		
+	if !flecha.aire:
+		if flecha.choca:
+			flecha.queue_free()
+			
+		flecha = arrow_scene.instantiate()
+		get_tree().current_scene.add_child(flecha)
+		flecha.global_position = global_position
+		if get_parent().has_method("conectar_extremo"):
+			get_parent().conectar_extremo(flecha)
+		var direccion_x = (get_global_mouse_position() - global_position).normalized()
+		print(str(direccion_x))
+		var fuerza_base = 6
+		flecha.rotation = direccion_x.angle()
+		#EL OBJETO A CREAR NO DEBE TENER LA VELOCIDAD EN X DEFINIDA EN 0 O EN OTRO VALOR ESTRICTO, LA VELOCIDAD DE LA FLECHA SE DA AQUI
+		flecha.linear_velocity = direccion_x * fuerza_base * carga_disparo
+		carga = 0
+		
+		flecha.aire = true
--- lanzando-lanzando-triunfe-escalando/flecha.gd
+++ lanzando-lanzando-triunfe-escalando/flecha.gd
@@ -1,4 +1,9 @@
 extends RigidBody2D
 
+var aire :bool = false
+var choca :bool = false
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	aire = false
+	choca = true
 	set_deferred("freeze", true)
