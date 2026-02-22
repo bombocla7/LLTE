95108a062aae056dbce6ab5b251fbc571cdc2022
bombocla7 <jejtalyt@gmail.com>
2026-02-22 18:00:46 +0100

    Cadena a punto

--- lanzando-lanzando-triunfe-escalando/main_character.gd
+++ lanzando-lanzando-triunfe-escalando/main_character.gd
@@ -12,33 +12,42 @@ var flecha_disparada: bool = false
 
 var eslabonL
 var eslabonS
+var cadena = []
 
+var trepar
+var velocidad_trepado
 
 
 var flechaP
 
 var ult_eslabon
-var aux
+var primer_eslabon
 @onready var animar = $Animacion
 
 func _ready():
 	get_node("./Arco").connect("arco_disparado", on_arco_disparado)
 	connect("flecha_choca", on_flecha_choca)
+	trepar = false
+	velocidad_trepado = 0.0
 
 func _process(delta: float) -> void:
 	if (flecha_disparada):
-		if (global_position.distance_to(ult_eslabon.global_position) > 100):
+		if (global_position.distance_to(ult_eslabon.global_position) > 10):
 			eslabonL = eslabonL_scene.instantiate()
 			get_tree().current_scene.add_child(eslabonL)
 			eslabonL.set_mass(0.001)
 			var joint = eslabonL.get_node("PinJoint2D")
-			eslabonL.global_position = ult_eslabon.global_position - ((ult_eslabon.global_position - global_position).normalized() * 10)
+			eslabonL.global_position = ult_eslabon.global_position - ((ult_eslabon.global_position - global_position).normalized() * 5)
 			joint.set_node_a(ult_eslabon.get_path())
 			joint.set_exclude_nodes_from_collision(true)
 			#eslabonL.set_freeze_enabled(true)
+			if(ult_eslabon == flechaP):
+				primer_eslabon = eslabonL
 			ult_eslabon = eslabonL
-			aux = aux + 1
-			
+			cadena.append(eslabonL)
+		
+	elif(trepar):
+		acccion_trepar(delta)
 
 func _physics_process(delta):
 	#GRAVEDAD#
@@ -46,7 +55,7 @@ func _physics_process(delta):
 		velocity.y += delta*gravity
 
 	
-	if not is_climbing():
+	if trepar:
 		pass
 	else:
 		#Aumento de las velocidades de manera normal(sin animación)
@@ -60,14 +69,15 @@ func _physics_process(delta):
 func is_climbing() -> bool:
 	#Comprueba si el jugador está escalando la cuerda
 	#calcular el camino a seguir desde la posicion del personaje hasta la posicion 
-	return true
+	return trepar
 	
 
 func velocidades():
-	var dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.x = dir * SPEED
-	if Input.is_action_just_pressed("ui_salto") && is_on_floor():
-		velocity.y = JUMP_FORCE
+	if(not trepar):
+		var dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		velocity.x = dir * SPEED
+		if Input.is_action_just_pressed("ui_salto") && is_on_floor():
+			velocity.y = JUMP_FORCE
 		
 func animaciones():
 	if velocity.x > 0:
@@ -86,28 +96,33 @@ func animaciones():
 	elif not is_on_floor():
 		animar.play("jump")
 
+func borra_cadena_anterior():
+	for i in cadena.size():
+		cadena[i].queue_free()
+	cadena.clear()
+
 func on_arco_disparado(flecha):
 	flecha_disparada = true
 	flechaP = flecha
 	ult_eslabon = flecha
 	get_node(flecha.get_path()).connect("flecha_choca", on_flecha_choca)
-	aux = 0
+	borra_cadena_anterior()
 
 func on_flecha_choca():
 	flecha_disparada = false
-	ult_eslabon.global_position = global_position
+	trepar = true
 	ult_eslabon.set_freeze_enabled(true)
-	var joint = ult_eslabon.get_node("PinJoint2D")
+	var joint = joint_scene.instantiate()
+	self.add_child(joint)
 	joint.set_node_a(self.get_path())
+	joint.set_node_b(ult_eslabon.get_path())
 	joint.set_exclude_nodes_from_collision(true)
-
-
-
-func flecha_caida(body: RigidBody2D) -> void:
-	if body.linear_velocity.x < 0:
-		while is_on_floor():
-			velocity.x = -SPEED
-	else:
-		while is_on_floor():
-			velocity.x = SPEED
-	pass
+	joint.global_position = global_position
+	primer_eslabon.global_position = flechaP.global_position
+	
+func acccion_trepar(delta):
+	velocidad_trepado += delta * 0.4
+	global_position = global_position.lerp(flechaP.global_position, velocidad_trepado)
+	var posicion_final = (global_position - flechaP.global_position);
+	if(posicion_final.y <= 10 and posicion_final.x <= 10):
+		trepar = false
